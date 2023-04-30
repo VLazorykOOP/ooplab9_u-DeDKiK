e2012de9b0166756a2ff3659db6d21a6dd397b2f
Vasyl Deresh <81803610+DeDKiK@users.noreply.github.com>
2023-04-30 23:59:09 +0300

    Update OOPLab9T.cpp

--- OOPLab9T/OOPLab9T.cpp
+++ OOPLab9T/OOPLab9T.cpp
@@ -50,4 +50,49 @@ int main()
 
     return 0;
 
+        
+//     Завдання 3 
+
+struct Client {
+    int month;
+    int year;
+    int code;
+    int duration;
+};
+
+int main() {
+    std::vector<Client> clients = {
+        {1, 2022, 1, 2},
+        {2, 2022, 2, 1},
+        {1, 2022, 3, 3},
+        {3, 2022, 4, 2},
+        {2, 2022, 5, 3},
+        {3, 2022, 6, 2},
+        {1, 2023, 7, 4},
+        {2, 2023, 8, 3},
+        {3, 2023, 9, 2},
+        {1, 2023, 10, 5},
+        {2, 2023, 11, 3},
+        {3, 2023, 12, 2}
+    };
+
+    auto max_duration_client = std::max_element(clients.begin(), clients.end(),
+        [](const Client& c1, const Client& c2) {
+            if (c1.duration != c2.duration) {
+                return c1.duration < c2.duration;
+            }
+            else if (c1.year != c2.year) {
+                return c1.year < c2.year;
+            }
+            else {
+                return c1.month < c2.month;
+            }
+        });
+
+    std::cout << "Max duration: " << max_duration_client->duration << std::endl;
+    std::cout << "Month: " << max_duration_client->month << std::endl;
+    std::cout << "Year: " << max_duration_client->year << std::endl;
+    std::cout << "Code: " << max_duration_client->code << std::endl;
+
+    return 0;
 }
